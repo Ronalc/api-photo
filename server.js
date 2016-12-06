eaea84622ee60b81f63756e790323d2fbcdd6319
Ronalses Aguilar <ronalses.developer@gmail.com>
2016-12-05 21:24:34 -0400

    Eliminado de realtime

--- server.js
+++ server.js
@@ -3,16 +3,13 @@
   * Dependencias
 */
 const express = require('express')
-const redis = require('redis')
 const expresSession = require('express-session')
-const RedisStore = require('connect-redis')(expresSession)
 const http = require('http')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 const routerApp = require("./routerApp");
 const authSession = require('./middlewares/session')
-const realTime = require('./realTime')
 const UserCtrl = require('./controlers/User')
 
 const app = express()
@@ -35,23 +32,12 @@ console.log('----------Iniciando-----------');
 console.log('------------------------------')
 
 
-
-// redis
-const sessionMiddlewareRedis = expresSession({
-  store: new RedisStore({}),
-  secret: "super Secretisimo",
-  resave: true,
-  saveUninitialized: true
-})
-
-
 // conect BD
 mongoose.connect('mongodb://localhost/api-photo', (err,res) => {
   if(err) res.status(500).json({error:true, message:err.message})
   console.log('Connect to Database')
 })
 
-//realTime(server, sessionMiddlewareRedis)
 
 
 /*
